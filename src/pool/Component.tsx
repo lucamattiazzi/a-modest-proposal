bef49abd7e30cd6697e34741c4e266c0d4e0652d
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2020-04-10 03:00:19 +0200

    stuff

--- src/pool/Component.tsx
+++ src/pool/Component.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { mixSamples, generateSamples } from './lib'
 import { DIFFUSION, POOL_SIZE, SAMPLES_NUMBER, POOL_NUMBER } from './constants'
 import { renderRatio } from '../utils'
 import { Range } from '../Common'
-import { generateHeatmapData, GRID_SIZE } from './generate-heatmap'
+import { getHeatmapData } from './generate-heatmap'
+const Plotly = require('plotly.js-dist')
 
 export function Component() {
   const [diffusion, setDiffusion] = useState(DIFFUSION.default)
@@ -12,50 +13,23 @@ export function Component() {
   const [poolNumber, setPoolNumber] = useState(POOL_NUMBER.default)
   const [falsePositives, setFalsePositives] = useState(null)
 
-  const canvas = useRef<HTMLCanvasElement>()
-
   function onCompute() {
     const samples = generateSamples(samplesNumber, diffusion)
     const newFalsePositives = mixSamples(samples, poolSize, poolNumber)
     setFalsePositives(newFalsePositives)
   }
 
-  function resizeCanvas() {
-    if (!canvas.current) return
-    const parent = canvas.current.parentNode as HTMLDivElement
-    const { width, height } = parent.getBoundingClientRect()
-    const side = Math.min(width, height)
-    canvas.current.width = side
-    canvas.current.height = side
-  }
-
   useEffect(() => {
-    window.onresize = resizeCanvas
-    resizeCanvas()
+    getHeatmapData().then(plotlyData => {
+      Plotly.newPlot('heatmap', ...plotlyData)
+    })
   }, [])
 
-  useEffect(() => {
-    if (!canvas.current) return
-    const data = generateHeatmapData()
-    const ctx = canvas.current.getContext('2d')
-    const side = canvas.current.width
-    const cellSize = side / GRID_SIZE
-    for (const rowIdx in data) {
-      const row = data[rowIdx]
-      const x = cellSize * parseInt(rowIdx)
-      for (const cellIdx in row) {
-        const y = cellSize * parseInt(cellIdx)
-        const cell = row[cellIdx]
-        const bckgColor = cell.falsePositiveRatio * 255
-        const color = 255 - bckgColor
-        ctx.fillStyle = `rgb(${bckgColor}, 0, 0)`
-        ctx.fillRect(x, y, cellSize, cellSize)
-        ctx.fillStyle = `rgb(0, 255, 255)`
-        ctx.font = `${cellSize / 2}px arial`
-        ctx.fillText(cell.falsePositiveRatio.toFixed(2), x, y)
-      }
-    }
-  }, [canvas])
+  function refreshData() {
+    getHeatmapData(true).then(plotlyData => {
+      Plotly.newPlot('heatmap', ...plotlyData)
+    })
+  }
 
   return (
     <div className="w-100 h-100 flex flex-column items-center justify-around">
@@ -98,9 +72,7 @@ export function Component() {
           </div>
         </div>
       </div>
-      <div className="w-100 h-50 flex items-center justify-center">
-        <canvas className="bg-black-10" ref={canvas} />
-      </div>
+      <div className="w-100 h-50" id="heatmap" onClick={refreshData} />
     </div>
   )
 }
--- src/pool/generate-heatmap.ts
+++ src/pool/generate-heatmap.ts
@@ -1,36 +1,75 @@
+import { range } from 'lodash-es'
 import { generateSamples, mixSamples } from './lib'
+import { renderRatio } from '../utils'
 
-export interface Result {
-  diffusion: number
-  compression: number
-  falsePositiveRatio: number
+const GRID_SIZE = 50
+const SAMPLES_NUMBER = 100
+const POOL_SIZE = 5
+const DIFFUSION_RANGE = [0.01, 0.2]
+const COMPRESSION_RANGE = [0.01, 0.5]
+
+const baseConfig = {
+  type: 'heatmap',
+  showscale: false,
+  hoverinfo: 'text',
+  colorscale: [[0, '#00ffff'], [1, '#ff0000']],
 }
 
-export const GRID_SIZE = 20
-const SAMPLES_NUMBER = 200
-const POOL_SIZE = 5
-export const DIFFUSION_RANGE = [0, 1]
-export const COMPRESSION_RANGE = [0, 1]
+const layout = {
+  title: 'Simulations heatmap',
+  yaxis: {
+    title: 'Covid diffusion',
+  },
+  xaxis: {
+    title: 'Compression range',
+  },
+}
 
-export function generateHeatmapData(): Result[][] {
+function buildRowText(compression: number, diffusion: number, falsePositives: number): string {
+  const compressionText = `Compression: ${renderRatio(compression)}`
+  const diffusionText = `Diffusion: ${renderRatio(diffusion)}`
+  const falsePositiveText = `False Positives: ${renderRatio(falsePositives)}`
+  return [compressionText, diffusionText, falsePositiveText].join('<br>')
+}
+
+function generateHeatmapData(): any {
   const diffusionStep = (DIFFUSION_RANGE[1] - DIFFUSION_RANGE[0]) / GRID_SIZE
   const compressionStep = (COMPRESSION_RANGE[1] - COMPRESSION_RANGE[0]) / GRID_SIZE
-
-  const grid: Result[][] = []
+  const x = range(COMPRESSION_RANGE[0], COMPRESSION_RANGE[1], compressionStep).map(renderRatio)
+  const y = range(DIFFUSION_RANGE[0], DIFFUSION_RANGE[1], diffusionStep).map(renderRatio)
+  const z = []
+  const text = []
 
   for (let i = 0; i < GRID_SIZE; i++) {
     const row = []
+    const textRow = []
     const diffusion = DIFFUSION_RANGE[0] + diffusionStep * i
     for (let j = 0; j < GRID_SIZE; j++) {
       const compression = COMPRESSION_RANGE[0] + compressionStep * j
-      const poolNumber = Math.round(compression * SAMPLES_NUMBER)
+      const poolNumber = Math.round((1 - compression) * SAMPLES_NUMBER)
       const samples = generateSamples(SAMPLES_NUMBER, diffusion)
       const falsePositives = mixSamples(samples, POOL_SIZE, poolNumber)
       const falsePositiveRatio = falsePositives / SAMPLES_NUMBER
-      row.push({ diffusion, compression, falsePositiveRatio })
+      const rowText = buildRowText(compression, diffusion, falsePositiveRatio)
+      textRow.push(rowText)
+      row.push(falsePositiveRatio)
     }
-    grid.push(row)
+    z.push(row)
+    text.push(textRow)
+  }
+  return { x, y, z, text }
+}
+
+function retrieveHeatmapData(): Promise<any> {
+  return window.fetch('/simulations.json').then(res => res.json())
+}
+
+export async function getHeatmapData(fresh: boolean = false): Promise<any> {
+  const rawData = fresh ? generateHeatmapData() : await retrieveHeatmapData()
+  const data = {
+    ...baseConfig,
+    ...rawData,
   }
 
-  return grid
+  return [[data], layout]
 }
